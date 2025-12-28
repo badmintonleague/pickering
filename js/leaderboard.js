af27fde2fa5b6134537c213ca82cb51217db7a95
abbilashu <abbilash.u@gmail.com>
2025-12-27 20:14:19 -0500

    Create leaderboard.js

---
+++ js/leaderboard.js
@@ -0,0 +1,23 @@
+const leaderboardEl = document.getElementById("leaderboard");
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+menuBtn.onclick = () => menu.classList.toggle("hidden");
+
+apiGet("getRankings").then(data => {
+  leaderboardEl.innerHTML = "";
+
+  data
+    .sort((a, b) => b.winPct - a.winPct)
+    .forEach((p, i) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+        <strong>#${i + 1} Player ${p.playerId}</strong><br>
+        Win%: ${(p.winPct * 100).toFixed(1)}%<br>
+        GP: ${p.gamesPlayed}<br>
+        PF: ${p.pf} | PA: ${p.pa}
+      `;
+      leaderboardEl.appendChild(card);
+    });
+});
